4ece261f7b34dd308f2cc5b63ee39c9d8c23e47c
Vakul Gaur <gaurvakul25@gmail.com>
2026-04-26 00:17:54 +0530

    Add OTP-based verification during user signup

--- server.js
+++ server.js
@@ -95,7 +95,8 @@ main()
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
